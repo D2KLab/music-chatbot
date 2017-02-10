75d02f0e94b01e7cabe887bc819c677a6adb3884
System <none>
2017-02-10 18:12:53 +0000

    Import of howdyai/botkit-starter-slack

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -9,11 +9,19 @@ module.exports = function(controller) {
     var webserver = express();
     webserver.use(bodyParser.json());
     webserver.use(bodyParser.urlencoded({ extended: true }));
+
+    // import express middlewares that are present in /components/express_middleware
+    var normalizedPath = require("path").join(__dirname, "express_middleware");
+    require("fs").readdirSync(normalizedPath).forEach(function(file) {
+        require("./express_middleware/" + file)(webserver, controller);
+    });
+
     webserver.use(express.static('public'));
 
-    webserver.listen(process.env.PORT || 3000,  null, function() {
 
-        debug('Express webserver configured and listening !');
+    webserver.listen(process.env.PORT || 3000, null, function() {
+
+        debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
 
     });
 
@@ -22,6 +30,8 @@ module.exports = function(controller) {
     require("fs").readdirSync(normalizedPath).forEach(function(file) {
       require("./routes/" + file)(webserver, controller);
     });
+
+    controller.webserver = webserver;
 
     return webserver;
 
