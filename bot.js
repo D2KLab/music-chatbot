113ec9fee6a793d0b2c128ee16cb548de0cab840
Luca Lombardo <none>
2018-03-31 23:00:22 +0000

    🏦🌲 Checkpoint ./bot.js:782394/82

--- bot.js
+++ bot.js
@@ -142,9 +142,7 @@ slackController.hears(['works-by-artist'], 'direct_message, direct_mention, ment
         bot.reply(message, "I'm sorry, I can't find your artist. Try with '" + result[0][1] + "'.");
         // we must clear the context
         // sendClearContext(message['nlpResponse']['sessionId']);
-        console.log(message['nlpResponse']['result']['contexts']);
-        th
-        
+        console.log(message['nlpResponse']['result']['contexts']);      
       } else {
         bot.reply(message, message['fulfillment']['speech']);
       }
