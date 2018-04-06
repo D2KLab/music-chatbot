4c950b09959143a5e8b6baf579cf247c481b622a
Luca Lombardo <none>
2018-04-06 22:03:01 +0000

    🐢🍚 Checkpoint ./bot.js:778114/7691

--- bot.js
+++ bot.js
@@ -134,9 +134,6 @@ slackController.hears(['works-by-artist'], 'direct_message, direct_mention, ment
     // MISSING ARTIST NAME
     // - check for misspelling and ask for the most similar (over threshold)
     // - otherwise forward the question sent by Dialogflow ("for which artist?")
-    if (message["text"].equals("yes")) {
-      
-    }
     
     // Retrieve the mispelled string
     var misspelled = message.entities["any"];
@@ -149,6 +146,44 @@ slackController.hears(['works-by-artist'], 'direct_message, direct_mention, ment
       var result = mispellingSolver.get(misspelled);
       if (result != null) {
         
+        /******************************** /
+        if (message["text"] === "yes") {
+
+          // GET PARAMETERS
+          var artist = message.entities["doremus-artist-ext"];
+          var number = message.entities["number"];
+
+          // DEFAULT NUMBER VALUE (IN CASE IS NOT GIVEN)
+          if (isNaN(parseInt(number))) {
+            number = 10;
+          }
+
+          // JSON VERSION
+          var jsonQuery = "http://data.doremus.org/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Ftitle%0D%0AWHERE+%7B%0D%0A++%3Fexpression+a+efrbroo%3AF22_Self-Contained_Expression+%3B%0D%0A++++rdfs%3Alabel+%3Ftitle+.%0D%0A++%3FexpCreation+efrbroo%3AR17_created+%3Fexpression+%3B%0D%0A++++ecrm%3AP9_consists_of+%2F+ecrm%3AP14_carried_out_by+%3Fcomposer+.%0D%0A++%3Fcomposer+foaf%3Aname+%22" + artist + "%22%0D%0A%7D%0D%0AORDER+BY+rand%28%29%0D%0ALIMIT+" + number + "%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on"
+          var jsonQuery = "http://data.doremus.org/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Ftitle%0D%0AWHERE+%7B%0D%0A++%3Fexpression+a+efrbroo%3AF22_Self-Contained_Expression+%3B%0D%0A++++rdfs%3Alabel+%3Ftitle+.%0D%0A++%3FexpCreation+efrbroo%3AR17_created+%3Fexpression+%3B%0D%0A++++ecrm%3AP9_consists_of+%2F+ecrm%3AP14_carried_out_by+%3Fcomposer%0D%0A++VALUES+%28%3Fcomposer%29+%7B%0D%0A++++%28%3Chttp%3A%2F%2Fdata.doremus.org%2Fartist%2F" + artist + "%3E%29%0D%0A++%7D%0D%0A%0D%0A%7D%0D%0AORDER+BY+rand%28%29%0D%0ALIMIT+" + number + "%0D%0A&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on";
+
+          const request = require('request');
+          request(jsonQuery, (err, res, body) => {
+
+            if (err) { return console.log(err); }
+
+            // JSON PARSING
+            var json = JSON.parse(body)
+
+            // RESPONSE
+            var resp = "This is the list:\n";
+            json["results"]["bindings"].forEach(function(row) {
+              resp += ("  >  " + row["title"]["value"] + "\n");
+            });
+            bot.reply(message, resp);
+          });
+        }
+        else if ((message["text"] === "no")) {
+
+          bot.reply(message, "Ok, sorry for that! Retry if you want...");
+          sendClearContext(message['nlpResponse']['sessionId']);
+        }
+        else {
         var answer = "Did you mean " + result[0][1] + "?"
         bot.reply(message, answer);
         
