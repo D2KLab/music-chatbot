54fb769167a29da921a7dee8f8a75ef92e4e21b2
Glitch (doremus-2) <none>
2018-04-25 15:33:48 +0000

    🎉📮 Checkpoint ./bot.js:782394/13297

--- bot.js
+++ bot.js
@@ -47,8 +47,8 @@ slackController.middleware.receive.use(dialogflowMiddleware.receive);
 slackBot.startRTM();
 
 
-// WORKS-BY-ARTIST INTENT
-slackController.hears(['works-by-artist'], 'direct_message, direct_mention, mention', dialogflowMiddleware.hears, function(bot, message) {
+// WORKS-BY INTENT
+slackController.hears(['works-by'], 'direct_message, direct_mention, mention', dialogflowMiddleware.hears, function(bot, message) {
   
   // ACTION COMPLETE (the artist name has been provided)
   if (message['nlpResponse']['result']['actionIncomplete'] == false) {
@@ -135,6 +135,7 @@ slackController.hears(['works-by-artist'], 'direct_message, direct_mention, ment
   }
 });
 
+/*
 // WORKS-BY-ARTIST YES FOLLOW-UP
 slackController.hears(['works-by-artist - yes'], 'direct_message, direct_mention, mention', dialogflowMiddleware.hears, function(bot, message) {
   
@@ -200,6 +201,8 @@ slackController.hears(['works-by-artist - no'], 'direct_message, direct_mention,
   doQuery(artist, number, null, "", startyear, endyear, bot, message);
 
 });
+
+*/
 
 // DISCOVER ARTIST
 slackController.hears(['discover-artist'], 'direct_message, direct_mention, mention', dialogflowMiddleware.hears, function(bot, message) {
@@ -398,3 +401,84 @@ slackController.hears(['Default Fallback Intent'], 'direct_message, direct_menti
   
   bot.reply(message, message['fulfillment']['speech']);
 });
+
+
+// Set up an Express-powered webserver to expose oauth and webhook endpoints
+var webserver = require(__dirname + '/components/express_webserver.js')(slackController);
+
+if (!process.env.clientId || !process.env.clientSecret) {
+
+  // Load in some helpers that make running Botkit on Glitch.com better
+  require(__dirname + '/components/plugin_glitch.js')(slackController);
+
+  webserver.get('/', function(req, res){
+    res.render('installation', {
+      studio_enabled: slackController.config.studio_token ? true : false,
+      domain: req.get('host'),
+      protocol: req.protocol,
+      glitch_domain:  process.env.PROJECT_DOMAIN,
+      layout: 'layouts/default'
+    });
+  })
+
+  var where_its_at = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me/';
+  console.log('WARNING: This application is not fully configured to work with Slack. Please see instructions at ' + where_its_at);
+}else {
+
+  webserver.get('/', function(req, res){
+    res.render('index', {
+      domain: req.get('host'),
+      protocol: req.protocol,
+      glitch_domain:  process.env.PROJECT_DOMAIN,
+      layout: 'layouts/default'
+    });
+  })
+  // Set up a simple storage backend for keeping a record of customers
+  // who sign up for the app via the oauth
+  require(__dirname + '/components/user_registration.js')(slackController);
+
+  // Send an onboarding message when a new team joins
+  require(__dirname + '/components/onboarding.js')(slackController);
+
+  // Load in some helpers that make running Botkit on Glitch.com better
+  require(__dirname + '/components/plugin_glitch.js')(slackController);
+
+  // enable advanced botkit studio metrics
+  require('botkit-studio-metrics')(slackController);
+
+  var normalizedPath = require("path").join(__dirname, "skills");
+  require("fs").readdirSync(normalizedPath).forEach(function(file) {
+    require("./skills/" + file)(slackController);
+  });
+
+  // This captures and evaluates any message sent to the bot as a DM
+  // or sent to the bot in the form "@bot message" and passes it to
+  // Botkit Studio to evaluate for trigger words and patterns.
+  // If a trigger is matched, the conversation will automatically fire!
+  // You can tie into the execution of the script using the functions
+  // controller.studio.before, controller.studio.after and controller.studio.validate
+  if (process.env.studio_token) {
+      slackController.on('direct_message,direct_mention,mention', function(bot, message) {
+          slackController.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
+              if (!convo) {
+                  // no trigger was matched
+                  // If you want your bot to respond to every message,
+                  // define a 'fallback' script in Botkit Studio
+                  // and uncomment the line below.
+                  // controller.studio.run(bot, 'fallback', message.user, message.channel);
+              } else {
+                  // set variables here that are needed for EVERY script
+                  // use controller.studio.before('script') to set variables specific to a script
+                  convo.setVar('current_time', new Date());
+              }
+          }).catch(function(err) {
+              bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
+              //debug('Botkit Studio: ', err);
+          });
+      });
+  } else {
+      console.log('~~~~~~~~~~');
+      console.log('NOTE: Botkit Studio functionality has not been enabled');
+      console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
+  }
+}
