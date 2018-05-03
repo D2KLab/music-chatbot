9584eea592de37b2565c4588e77839ae6411aa73
claudioscalzo <none>
2018-05-03 18:36:11 +0000

    🕵👊 Checkpoint ./bot_functions.js:778114/5024 ./slack_cards.js:778114/4677

--- bot_functions.js
+++ bot_functions.js
@@ -1,5 +1,6 @@
 // LOAD VARIABLES
 var botvars = require("./bot_vars.js");
+var slackcards = 
 var misspellingSolver = botvars.misspellingSolver;
 var popularityDictionary = botvars.popularityDictionary;
 var slackBot = botvars.slackBot;
@@ -7,7 +8,7 @@ var slackBot = botvars.slackBot;
 
 // FUNCTIONS
 /*******************************************************************************/
-var sendClearContext = function(sessionID) {
+function sendClearContext(sessionID) {
   
   var request = require('request');
   var options = {
@@ -26,163 +27,6 @@ var sendClearContext = function(sessionID) {
     }
   }
   request(options, callback)
-}
-/*******************************************************************************/
-
-
-/*******************************************************************************/
-var getBioCard = function(fullname, birthPlace, birthDate, deathPlace, deathDate, imageURL, bio) {
-  var imageURLHTTPDropped = imageURL.split("://")[1]
-  var bioAttachment = {
-    "attachments": [{
-        "pretext": "This is what I found:",
-        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
-        "title" : fullname,
-        "image_url": "https://rsz.io/" + imageURLHTTPDropped + "?mode=crop&width=150&height=150",
-        "fields": [
-            {
-                "title": "Born in",
-                "value": birthPlace,
-                "short": true
-            },
-            {
-                "title": "Birthdate",
-                "value": birthDate,
-                "short": true
-            },
-            {
-                "title": "Dead in",
-                "value": deathPlace,
-                "short": true
-            },
-            {
-                "title": "Death date",
-                "value": deathDate,
-                "short": true
-            },
-            {
-                "title": "Bio",
-                "value": bio, 
-                "short": false
-            }
-        ],
-        "color": "good"
-    }]
-  }
-  return bioAttachment;
-}
-/*******************************************************************************/
-
-/*******************************************************************************/
-var getArtistCard = function(fullname, birthPlace, birthDate, deathPlace, deathDate, count) {
-
-  var artistAttachment = {
-    "attachments": [{
-        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
-        "title" : fullname,
-        "fields": [
-            {
-                "title": "Born in",
-                "value": birthPlace,
-                "short": true
-            },
-            {
-                "title": "Birthdate",
-                "value": birthDate,
-                "short": true
-            },
-            {
-                "title": "Dead in",
-                "value": deathPlace,
-                "short": true
-            },
-            {
-                "title": "Death date",
-                "value": deathDate,
-                "short": true
-            },
-            {
-                "title": "Number of works",
-                "value": count,
-                "short": true
-            }
-        ],
-        "color": "good"
-    }]
-  }
-  return artistAttachment;
-}
-/*******************************************************************************/
-
-/*******************************************************************************/
-var getWorkCard = function(title, artist, year, genre, comment, key) {
-  var workAttachment = {
-    "attachments": [{
-        "title": title,
-        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
-        "fields": [
-            {
-                "title": "Artist",
-                "value": artist,
-                "short": true
-            },
-            {
-                "title": "Year",
-                "value": year,
-                "short": true
-            },
-            {
-                "title": "Genre",
-                "value": genre,
-                "short": true
-            },
-            {
-                "title": "Key",
-                "value": key,
-                "short": true
-            },
-            {
-                "title": "Comment",
-                "value": comment,
-                "short": false
-            }
-        ],
-        "color": "#4283f4"
-    }]
-  }
-  return workAttachment;
-}
-/*******************************************************************************/
-
-
-/*******************************************************************************/
-var getPerformanceCard = function(title, subtitle, placeName, actorsName, date) {
-  var performanceAttachment = {
-    "attachments": [{
-        "title": title,
-        "text": subtitle,
-        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
-        "fields": [
-            {
-                "title": "Where",
-                "value": placeName,
-                "short": true
-            },
-            {
-                "title": "When",
-                "value": date,
-                "short": true
-            },
-            {
-                "title": "Actors",
-                "value": actorsName,
-                "short": false
-            }
-        ],
-        "color": "#f4b042"
-    }]
-  }
-  return performanceAttachment;
 }
 /*******************************************************************************/
 
---
+++ slack_cards.js
@@ -0,0 +1,155 @@
+/*******************************************************************************/
+module.exports = function getBioCard(fullname, birthPlace, birthDate, deathPlace, deathDate, imageURL, bio) {
+  var imageURLHTTPDropped = imageURL.split("://")[1]
+  var bioAttachment = {
+    "attachments": [{
+        "pretext": "This is what I found:",
+        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
+        "title" : fullname,
+        "image_url": "https://rsz.io/" + imageURLHTTPDropped + "?mode=crop&width=150&height=150",
+        "fields": [
+            {
+                "title": "Born in",
+                "value": birthPlace,
+                "short": true
+            },
+            {
+                "title": "Birthdate",
+                "value": birthDate,
+                "short": true
+            },
+            {
+                "title": "Dead in",
+                "value": deathPlace,
+                "short": true
+            },
+            {
+                "title": "Death date",
+                "value": deathDate,
+                "short": true
+            },
+            {
+                "title": "Bio",
+                "value": bio, 
+                "short": false
+            }
+        ],
+        "color": "good"
+    }]
+  }
+  return bioAttachment;
+}
+/*******************************************************************************/
+
+/*******************************************************************************/
+module.exports = function getArtistCard(fullname, birthPlace, birthDate, deathPlace, deathDate, count) {
+
+  var artistAttachment = {
+    "attachments": [{
+        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
+        "title" : fullname,
+        "fields": [
+            {
+                "title": "Born in",
+                "value": birthPlace,
+                "short": true
+            },
+            {
+                "title": "Birthdate",
+                "value": birthDate,
+                "short": true
+            },
+            {
+                "title": "Dead in",
+                "value": deathPlace,
+                "short": true
+            },
+            {
+                "title": "Death date",
+                "value": deathDate,
+                "short": true
+            },
+            {
+                "title": "Number of works",
+                "value": count,
+                "short": true
+            }
+        ],
+        "color": "good"
+    }]
+  }
+  return artistAttachment;
+}
+/*******************************************************************************/
+
+/*******************************************************************************/
+module.exports = function getWorkCard(title, artist, year, genre, comment, key) {
+  var workAttachment = {
+    "attachments": [{
+        "title": title,
+        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
+        "fields": [
+            {
+                "title": "Artist",
+                "value": artist,
+                "short": true
+            },
+            {
+                "title": "Year",
+                "value": year,
+                "short": true
+            },
+            {
+                "title": "Genre",
+                "value": genre,
+                "short": true
+            },
+            {
+                "title": "Key",
+                "value": key,
+                "short": true
+            },
+            {
+                "title": "Comment",
+                "value": comment,
+                "short": false
+            }
+        ],
+        "color": "#4283f4"
+    }]
+  }
+  return workAttachment;
+}
+/*******************************************************************************/
+
+
+/*******************************************************************************/
+module.exports = function getPerformanceCard(title, subtitle, placeName, actorsName, date) {
+  var performanceAttachment = {
+    "attachments": [{
+        "title": title,
+        "text": subtitle,
+        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
+        "fields": [
+            {
+                "title": "Where",
+                "value": placeName,
+                "short": true
+            },
+            {
+                "title": "When",
+                "value": date,
+                "short": true
+            },
+            {
+                "title": "Actors",
+                "value": actorsName,
+                "short": false
+            }
+        ],
+        "color": "#f4b042"
+    }]
+  }
+  return performanceAttachment;
+}
+/*******************************************************************************/
