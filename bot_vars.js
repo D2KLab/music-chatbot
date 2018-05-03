cb0303754114c7fcc8dd30e07a895da2eb0114d3
claudioscalzo <none>
2018-05-03 11:53:35 +0000

    🐀👱 Checkpoint ./bot_vars.js:782394/956

--- bot_vars.js
+++ bot_vars.js
@@ -4,19 +4,41 @@ var FuzzySet = require('fuzzyset.js');
 var request = require('request');
 var http = require('http');
 var SpellChecker = require('spellchecker')
-var bot_options = {
+
+/* SLACK */
+var slackBotOptions = {
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret,
     //debug: true,
     scopes: ['bot'],
 };
-var slackController = Botkit.slackbot(bot_options);
+var slackController = Botkit.slackbot(slackBotOptions);
 var slackBot = slackController.spawn({
     token: process.env.token,
 });
+
+
+
+/* FB MESSENGER */ 
+var fbBotOptions = {
+  debug: true,
+  log: true,
+  access_token: process.env.fbAccessToken,
+  verify_token: process.env.fbVerifyToken,
+  app_secret: process.env.fbAppSecret,
+  validate_requests: true
+};
+
+var fbController = Botkit.facebookbot(fbBotOptions);
+var fbBot = fbController.spawn({
+  
+});
+
 var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
     token: process.env.dialogflow,
 });
+
+
 
 // EXPORTS
 exports.slackController = slackController;
