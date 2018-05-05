d2ff25d25e3d8c53701d63ba21f5a7ba92266c2a
claudioscalzo <none>
2018-05-05 08:43:45 +0000

    🏍🐸 Checkpoint ./doremus/bot_functions.js:782394/627 ./facebook/facebook_cards.js:782394/1785

--- facebook/facebook_cards.js
+++ facebook/facebook_cards.js
@@ -40,35 +40,25 @@ module.exports.getBioCard = function getBioCard(fullname, birthPlace, birthDate,
     }]
   }
   var attachment = {
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome!",
-            "image_url":"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We have the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://petersfancybrownhats.com/view?item=103",
-              "messenger_extensions": false,
-              "webview_height_ratio": "tall",
-              "fallback_url": "https://petersfancybrownhats.com/"
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
-            ]      
-          }
-        ]
-      }
-    }
+        'type':'template',
+        'payload':{
+            'template_type':'generic',
+            'elements':[
+                {
+                    'title': fullname,
+                    'image_url': "https://rsz.io/" + imageURLHTTPDropped + "?mode=crop&width=150&height=150",
+                    'subtitle':'A delicious chocolate cookie',
+                    
+                    'buttons':[
+                        {
+                        'type':'postback',
+                        'title':'Eat Cookie',
+                        'payload':'chocolate'
+                        }
+                    ]
+                },
+            ]
+        }
+    };
   return attachment;
 }
