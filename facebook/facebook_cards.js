04042bb3907463b88cf7f5af1cc3a0409b8b19c9
claudioscalzo <none>
2018-05-05 08:54:43 +0000

    😋🦁 Checkpoint ./facebook/facebook_cards.js:782394/1433

--- facebook/facebook_cards.js
+++ facebook/facebook_cards.js
@@ -4,42 +4,6 @@
 module.exports.getBioCard = function getBioCard(fullname, birthPlace, birthDate, deathPlace, deathDate, imageURL, bio) {
   var imageURLHTTPDropped = imageURL.split("://")[1]
   var bioAttachment = {
-    "attachments": [{
-        "pretext": "This is what I found:",
-        "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
-        "title" : fullname,
-        "image_url": "https://rsz.io/" + imageURLHTTPDropped + "?mode=crop&width=150&height=150",
-        "fields": [
-            {
-                "title": "Born in",
-                "value": birthPlace,
-                "short": true
-            },
-            {
-                "title": "Birth date",
-                "value": birthDate,
-                "short": true
-            },
-            {
-                "title": "Dead in",
-                "value": deathPlace,
-                "short": true
-            },
-            {
-                "title": "Death date",
-                "value": deathDate,
-                "short": true
-            },
-            {
-                "title": "Bio",
-                "value": bio, 
-                "short": false
-            }
-        ],
-        "color": "good"
-    }]
-  }
-  var attachment = {
         'type':'template',
         'payload':{
             'template_type':'generic',
@@ -47,12 +11,14 @@ module.exports.getBioCard = function getBioCard(fullname, birthPlace, birthDate,
                 {
                     'title': fullname,
                     'image_url': "https://rsz.io/" + imageURLHTTPDropped + "?mode=crop&width=150&height=150",
-                    'subtitle':'A delicious chocolate cookie',
-                    
+                    'subtitle':'Born in: ' + birthPlace +
+                               '\nBirth date: ' + birthDate +
+                               '\nDeath in :' + deathPlace +
+                               '\nDeath date: ' + deathPlace,                
                     'buttons':[
                         {
                         'type':'postback',
-                        'title':'Eat Cookie',
+                        'title':'Get bio',
                         'payload':'chocolate'
                         }
                     ]
@@ -60,5 +26,5 @@ module.exports.getBioCard = function getBioCard(fullname, birthPlace, birthDate,
             ]
         }
     };
-  return attachment;
+  return bioAttachment;
 }
