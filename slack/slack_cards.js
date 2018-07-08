3eaa17dd8986ecbe21016bf9b3793e53b90d8b40
Claudio Scalzo <claudio.scalzo@outlook.com>
2018-07-08 14:59:20 +0200

    Translated in french the help intent.

--- slack/slack_cards.js
+++ slack/slack_cards.js
@@ -132,7 +132,7 @@ module.exports.getHelpCard = function getHelpCard(language) {
                     "value": "Once you know the name of an artist, you can get the bio.",
                     "short": false
                 }, {
-                    "title": "Now give me 5 of his works, written for clarinet",
+                    "title": "... now give me 5 of his works, written for clarinet",
                     "value": "Hey, I remember when we are talking about an artist!",
                     "short": false
                 }, {
@@ -146,35 +146,31 @@ module.exports.getHelpCard = function getHelpCard(language) {
     } else if (language === 'fr') {
         helpAttachment = {
             "attachments": [{
-                "title": "Some things you can ask me:",
-                "fallback": "New message from DOREMUS Bot!",
+                "title": "Tu me peux demander:",
+                "fallback": "Nouveau message de DOREMUS Bot!",
                 "fields": [{
-                    "title": "Give me 3 works composed by Bach",
-                    "value": "You can find the WORKS with your preferred filters.",
+                    "title": "Donne-moi deux oeuvres de Bach",
+                    "value": "Tu peux trouver les oeuvres avec tes filtres favoris.",
                     "short": false
                 }, {
-                    "title": "Give me 2 works for violin, clarinet and piano",
-                    "value": "Not only by artist but also by multiple instruments!",
+                    "title": "Donne-moi deux oeuvres pour violin, clarinette et piano",
+                    "value": "Aussi avec plusieurs instruments!",
                     "short": false
                 }, {
-                    "title": "Tell me 5 sonatas written between 1782 and 1821",
-                    "value": "And even more: genre and composition year.",
+                    "title": "Trouve-moi un compositeur qui a écrit plus oeuvres pour clarinette",
+                    "value": "Tu peux trouver les compositeurs avec des filtres!",
                     "short": false
                 }, {
-                    "title": "Find one artist, born between 1752 and 1772, who wrotes more works for clarinet",
-                    "value": "You can find the ARTISTS with your preferred filters",
+                    "title": "Parle-moi de Beethoven",
+                    "value": "Tu peux obtenir le bio d'un compositeur",
                     "short": false
                 }, {
-                    "title": "What do you know about Beethoven?",
-                    "value": "Once you know the name of an artist, you can get the bio.",
+                    "title": "... alors donne-moi deux de ses aria pour violin et clarinette",
+                    "value": "Hey, je me souviens qu'on parle d'un compositeur!",
                     "short": false
                 }, {
-                    "title": "Now give me 5 of his works, written for clarinet",
-                    "value": "Hey, I remember when we are talking about an artist!",
-                    "short": false
-                }, {
-                    "title": "Tell me one event in Paris in the next month!",
-                    "value": "I'm also good at discovering EVENTS in your favourite places",
+                    "title": "Proposes-moi un évènement le mois prochain",
+                    "value": "Je suis aussi capable de trouver des performances de musique!",
                     "short": false
                 }],
                 "color": "#4283f4"
